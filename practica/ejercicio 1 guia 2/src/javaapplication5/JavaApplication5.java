a54c66f428b9799a0f90d7dc947dcaded60f1cad
Christian_Gabriel_Franco_Lewicki <gabriel.lewicki.cgfl@gmail.com>
2024-12-28 03:39:32 -0300

    Update ejercicio 1.java

--- practica/ejercicio 1 guia 2/src/javaapplication5/JavaApplication5.java
+++ practica/ejercicio 1 guia 2/src/javaapplication5/JavaApplication5.java
@@ -1,25 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 //Escribir un programa que pida dos números enteros por teclado y calcule la suma de los dos. 
 //El programa deberá después mostrar el resultado de la suma
-
-
 package javaapplication5;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Martin
- */
 public class JavaApplication5 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int resultado, numero1,numero2;
         String num1, num2;
